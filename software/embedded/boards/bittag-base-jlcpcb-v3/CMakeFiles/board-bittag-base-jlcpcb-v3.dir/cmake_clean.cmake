fde7451814e32bc4ea6bb6446ad063704bff99e5
Geoffrey Brown <geobrown@indiana.edu>
2023-02-25 16:40:04 -0500

    added v3 board

---
+++ software/embedded/boards/bittag-base-jlcpcb-v3/CMakeFiles/board-bittag-base-jlcpcb-v3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/board-bittag-base-jlcpcb-v3"
+  "board.c"
+  "board.h"
+  "board.mk"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/board-bittag-base-jlcpcb-v3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
