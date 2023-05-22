d63e4cea79b4cc1e17fe62c52dd6a2e78a2f8088
Geoffrey Brown <geobrown@indiana.edu>
2023-05-22 14:33:37 -0400

    unwind small change

--- software/embedded/tags/BitTagv5/cfg/mcuconf.h
+++ software/embedded/tags/BitTagv5/cfg/mcuconf.h
@@ -194,8 +194,8 @@
 /*
  * RTC driver system settings.
  */
-#define STM32_RTC_PRESA_VALUE               1
-#define STM32_RTC_PRESS_VALUE               1024
+#define STM32_RTC_PRESA_VALUE               4
+#define STM32_RTC_PRESS_VALUE               256
 #define STM32_RTC_CR_INIT                   0
 #define STM32_RTC_TAMPCR_INIT               0
 
