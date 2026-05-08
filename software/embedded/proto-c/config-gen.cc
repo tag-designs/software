8e1874f2d8f5505a63507665ba2f790be0649a16
Geoffrey Brown <geoffreymbrown@gmail.com>
2026-05-08 19:20:53 -0400

    Codex rewrite of cmake files

--- software/embedded/proto-c/config-gen.cc
+++ software/embedded/proto-c/config-gen.cc
@@ -14,7 +14,7 @@ using namespace google::protobuf::util;
 int main(int argc, char *argv[]) {
     Config configin;
     if (argc < 2) {
-        cerr << "Usage: " << argv[0] << " json-file" << endl;
+        cerr << "Usage: " << argv[0] << " json-file [output-file]" << endl;
         return 1;
     } else {
         ifstream fin(argv[1]);
@@ -32,23 +32,33 @@ int main(int argc, char *argv[]) {
         google::protobuf::util::JsonParseOptions options2;
         auto err = JsonStringToMessage(str, &configin, options2);
         if (err.ok()) {
+            ofstream fout;
+            ostream *out = &cout;
+            if (argc > 2) {
+                fout.open(argv[2]);
+                if (!fout.is_open()) {
+                    cerr << "Couldn't open " << argv[2] << endl;
+                    return 1;
+                }
+                out = &fout;
+            }
+
             string output;
             configin.SerializeToString(&output);
-            //cout << "// generated output len : " << output.length() << endl;
-            cout << "const unsigned char ";
-            cout << "tag_default_config[" << output.length() << "] = {\n   ";
+            *out << "const unsigned char ";
+            *out << "tag_default_config[" << output.length() << "] = {\n   ";
             int i;
             for (i = 0; i < output.length(); i++) {
-                cout << (unsigned int) (output[i] & 0xff);
+                *out << (unsigned int) (output[i] & 0xff);
                 if (i+1 < output.length()) {
-                    cout << ", ";
+                    *out << ", ";
                     if ((i % 10 ) == 9) {
-                        cout << "\n   ";
+                        *out << "\n   ";
                     }
                 }
             }
-            cout << "\n};\n";
-            cout << "const unsigned int tag_default_config_len = " << output.length() << ";\n";
+            *out << "\n};\n";
+            *out << "const unsigned int tag_default_config_len = " << output.length() << ";\n";
         } else {
             cerr << err << std::endl;
         }
